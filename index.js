6804d662466462a850e728e3cbcf8acd8d480f64
thehydroimpulse <dnfagnan@gmail.com>
2016-04-19 15:58:29 -0700

    tests pass

--- index.js
+++ index.js
@@ -8,6 +8,63 @@ function isObject(o) {
   return false;
 }
 
+exports = module.exports = trample;
+exports.flattenUntilArrayLeaf = flattenUntilArrayLeaf;
+
+function flattenUntilArrayLeaf(key, args, obj) {
+  if (!obj) obj = {};
+
+  let isLeaf = false;
+
+  for (let i = 0; i < args.input.length; i++) {
+    const prop = args.input[i];
+    const arrayKey = (!key) ? i : (key + '.' + i);
+
+    if (isObject(prop)) {
+      isLeaf = false;
+      merge(obj, getRoot(arrayKey, prop, args.options));
+    } else if (prop instanceof Array && args.options.flattenArray) {
+      isLeaf = false;
+      let arr = flattenUntilArrayLeaf(arrayKey, {
+        input: prop,
+        options: args.options
+      }, obj);
+
+    } else if (!args.options.flattenArray && prop instanceof Array) {
+      isLeaf = false;
+      let guard = false;
+
+      for (let k = 0; k < prop.length; k++) {
+        let kval = prop[k];
+
+        if (isObject(kval) || kval instanceof Array) {
+          guard = false;
+          merge(obj, flattenUntilArrayLeaf(arrayKey, {
+            input: prop,
+            options: args.options
+          }, obj));
+          break;
+        } else {
+          guard = true;
+        }
+      }
+
+      if (guard) {
+        obj[arrayKey] = prop;
+      }
+    } else {
+      isLeaf = true;
+      obj[arrayKey] = prop;
+    }
+  }
+
+  if (isLeaf && !args.options.flattenArray) {
+    return args.input;
+  }
+
+  return obj;
+}
+
 /**
  * ```js
  * trample({}, { flattenArray: true });
@@ -15,38 +72,48 @@ function isObject(o) {
  */
 function trample(props, options) {
   if (!options) options = {};
-
   let obj = {};
 
-  // options.flattenArray
-  function getRoot(localKey, topProps) {
-    for (let key in topProps) {
-      if (topProps.hasOwnProperty(key)) {
-        const newKey = (!localKey) ? key : localKey + '.' + key;
-        const value = topProps[key];
-
-        if (isObject(value)) {
-          getRoot(newKey, value);
-        } else if (options.flattenArray && value instanceof Array) {
-          value.map((prop, i) => {
-            const arrayKey = newKey + '.' + i;
-
-            if (isObject(prop)) {
-              getRoot(arrayKey, prop);
-            } else {
-              obj[arrayKey] = prop;
-            }
-          });
-        } else {
-          obj[newKey] = value;
-        }
-      }
-    }
-  }
-
-  getRoot(null, props);
+  merge(obj, getRoot(null, props, options));
 
   return obj;
 }
 
-module.exports = trample;
+function merge(one, two) {
+  for (let key in two) {
+    let value = two[key];
+    one[key] = value;
+  }
+}
+
+function getRoot(localKey, props, options) {
+  let obj = {};
+
+  for (let key in props) {
+    if (props.hasOwnProperty(key)) {
+      const newKey = (localKey === null) ? key : localKey + '.' + key;
+      const value = props[key];
+
+      if (isObject(value)) {
+        merge(obj, getRoot(newKey, value, options));
+      } else if (value instanceof Array) {
+        let arr = flattenUntilArrayLeaf(newKey, {
+          input: value,
+          options: options
+        });
+
+        if (arr instanceof Array) {
+          obj[newKey] = arr;
+        } else {
+          merge(obj, arr);
+        }
+
+      } else {
+        obj[newKey] = value;
+      }
+    }
+  }
+
+  return obj;
+}
+
--- test/index.js
+++ test/index.js
@@ -1,5 +1,94 @@
 const assert = require('assert');
 const trample = require('..');
+const getLeaf = trample.flattenUntilArrayLeaf;
+
+describe('flatten array leaf', function() {
+  it('should return leaf node', function() {
+    var res = getLeaf(null, {
+      input: ['Hello', 'World'],
+      options: { flattenArray: false }
+    });
+
+    assert.deepEqual(res, ['Hello', 'World']);
+  });
+
+  it('should flatten leaf nodes including array', function() {
+    var res = getLeaf(null, {
+      input: ['Hello', 'World'],
+      options: { flattenArray: true }
+    });
+
+    assert(!(res instanceof Array));
+    assert.deepEqual(res, { '0': 'Hello', '1': 'World' });
+  });
+
+  it('should not flatten the leaf nodes (array)', function() {
+    var res = getLeaf(null, {
+      input: ['Hello', ['Foobar']],
+      options: { flattenArray: false }
+    });
+
+    assert(!(res instanceof Array));
+    assert.deepEqual(res, {
+      '0': 'Hello',
+      '1': ['Foobar']
+    });
+  });
+
+  it('should flatten until leaf node array', function() {
+    var res = getLeaf(null, {
+      input: ['Hello', ['Foobar', ['Big']]],
+      options: { flattenArray: false }
+    });
+
+    assert(!(res instanceof Array));
+    assert.deepEqual(res, {
+      '0': 'Hello',
+      '1.0': 'Foobar',
+      '1.1': ['Big']
+    });
+  });
+
+  it('should flatten deep nested arrays', function() {
+    var res = getLeaf(null, {
+      input: ['Hello', ['Foobar', ['Big']]],
+      options: { flattenArray: true }
+    });
+
+    assert(!(res instanceof Array));
+    assert.deepEqual(res, {
+      '0': 'Hello',
+      '1.0': 'Foobar',
+      '1.1.0': 'Big'
+    });
+  });
+
+  it('should flatten everything', function() {
+    var res = getLeaf(null, {
+      input: ['Hello', ['Foobar']],
+      options: { flattenArray: true }
+    });
+
+    assert(!(res instanceof Array));
+    assert.deepEqual(res, {
+      '0': 'Hello',
+      '1.0': 'Foobar'
+    });
+  });
+
+  it('should flatten parent array with child object.', function() {
+    var res = getLeaf(null, {
+      input: [{
+        foo: { bar: 123 }
+      }],
+      options: { flattenArray: false }
+    });
+
+    assert.deepEqual(res, {
+      '0.foo.bar': 123
+    });
+  });
+});
 
 describe('trample', function() {
   it('should return a function', function() {
@@ -51,37 +140,45 @@ describe('trample', function() {
 
     const res = trample(obj);
 
-    assert.equal(res['products'][0].orderId, 75);
-    assert.equal(res['products'][1].orderId, 32);
+    assert.equal(res['products.0.orderId'], 75);
+    assert.equal(res['products.1.orderId'], 32);
 
     assert.equal(res['foo.bar'][0], 'Hello');
     assert.equal(res['foo.bar'][1], 'World');
 
     assert.equal(res['address.state.a'], 'ca');
     assert.equal(res['address.state.b'], 'sf');
-    assert.equal(res['address.cat'][0].name, 'John Doe');
+    assert.equal(res['address.cat.0.name'], 'John Doe');
   });
 
   it('should flatten complex object w/ tags', function() {
     const obj = {
       products: [
-        { orderId: 75, tags: ['one', 'two'] },
+        {
+          orderId: 75, tags: ['one', 'two'],
+          foo: {
+            bar: 123
+          }
+        },
         { orderId: 32 }
       ],
     };
 
     const res = trample(obj);
 
-    assert.equal(res['products'][0].orderId, 75);
-    assert.equal(res['products'][0].tags[0], 'one');
-    assert.equal(res['products'][0].tags[1], 'two');
-    assert.equal(res['products'][1].orderId, 32);
+    assert.equal(res['products.0.orderId'], 75);
+    assert.equal(res['products.0.tags'][0], 'one');
+    assert.equal(res['products.0.tags'][1], 'two');
+    assert.equal(res['products.1.orderId'], 32);
   });
 
   it('should flatten complex object w/ tags w/ flatten array', function() {
     const obj = {
       products: [
-        { orderId: 75, tags: ['one', 'two'] },
+        {
+          orderId: 75,
+          tags: ['one', 'two']
+        },
         { orderId: 32 }
       ],
     };
